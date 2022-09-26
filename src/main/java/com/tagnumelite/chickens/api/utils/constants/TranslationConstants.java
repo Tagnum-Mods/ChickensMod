0fc6eebdf0c91d8b0d95d056d2e8f4479ecfe86b
Tagan Hoyle <tagnum.elite@gmail.com>
2022-09-26 20:29:07 +0200

    Switch to constants for translations
    Also, removed Lay Progress from jade as that is added by Chicken super

---
+++ src/main/java/com/tagnumelite/chickens/api/utils/constants/TranslationConstants.java
@@ -0,0 +1,18 @@
+package com.tagnumelite.chickens.api.utils.constants;
+
+public class TranslationConstants {
+    public static final String ANALYZER_TOOLTIP_1 = "item.chickens.analyzer.tooltip1";
+    public static final String ANALYZER_TOOLTIP_2 = "item.chickens.analyzer.tooltip2";
+    public static final String HENHOUSE_TOOLTIP = "henhouse.tooltip";
+    public static final String JEI_THROWING = "gui.chickens.jei.throwing";
+    public static final String JEI_BREEDING = "gui.chickens.jei.breeding";
+    public static final String JEI_LAYING = "gui.chickens.jei.breeding";
+    public static final String FLUID_EGG_TOOLTIP = "item.chickens.fluid_egg.tooltip"; //It's like one-off bucket.
+     public static final String CHICKEN_LAY_PROGRESS = "entity.chickens.chicken.lay_progress";
+     public static final String CHICKEN_NEXT_EGG_SOON = "entity.chickens.chicken.next_egg_soon";
+     public static final String CHICKEN_STATS_TIER = "entity.chickens.chicken.tier";
+     public static final String CHICKEN_STATS_GROWTH = "entity.chickens.chicken.growth"; //
+     public static final String CHICKEN_STATS_GAIN = "entity.chickens.chicken.gain"; //
+     public static final String CHICKEN_STATS_STRENGTH = "entity.chickens.chicken.strength"; //
+
+}
--- src/main/java/com/tagnumelite/chickens/integration/jade/ChickensJadePlugin.java
+++ src/main/java/com/tagnumelite/chickens/integration/jade/ChickensJadePlugin.java
@@ -1,6 +1,7 @@
 package com.tagnumelite.chickens.integration.jade;
 
 import com.tagnumelite.chickens.api.utils.Utils;
+import com.tagnumelite.chickens.api.utils.constants.TranslationConstants;
 import com.tagnumelite.chickens.common.entities.ChickensChicken;
 import com.tagnumelite.chickens.config.ClientConfig;
 import net.minecraft.network.chat.Component;
@@ -22,21 +23,12 @@ public class ChickensJadePlugin implements IWailaPlugin {
         @Override
         public void appendTooltip(ITooltip iTooltip, EntityAccessor entityAccessor, IPluginConfig iPluginConfig) {
             if (entityAccessor.getEntity() instanceof ChickensChicken chicken) {
-                iTooltip.add(Component.translatable("entity.chickens_chicken.tier", chicken.getTier()));
+                iTooltip.add(Component.translatable(TranslationConstants.CHICKEN_STATS_TIER, chicken.getTier()));
 
                 if (chicken.getStatsAnalyzed() || ClientConfig.ALWAYS_SHOW_STATS.get()) {
-                    iTooltip.add(Component.translatable("entity.ChickensChicken.growth", chicken.getGrowth()));
-                    iTooltip.add(Component.translatable("entity.ChickensChicken.gain", chicken.getGain()));
-                    iTooltip.add(Component.translatable("entity.ChickensChicken.strength", chicken.getStrength()));
-                }
-
-                if (!chicken.isBaby()) {
-                    int layProgress = chicken.getLayProgress();
-                    if (layProgress <= 0) {
-                        iTooltip.add(Component.translatable("entity.ChickensChicken.nextEggSoon"));
-                    } else {
-                        iTooltip.add(Component.translatable("entity.ChickensChicken.layProgress", layProgress));
-                    }
+                    iTooltip.add(Component.translatable(TranslationConstants.CHICKEN_STATS_GROWTH, chicken.getGrowth()));
+                    iTooltip.add(Component.translatable(TranslationConstants.CHICKEN_STATS_GAIN, chicken.getGain()));
+                    iTooltip.add(Component.translatable(TranslationConstants.CHICKEN_STATS_STRENGTH, chicken.getStrength()));
                 }
             }
         }
